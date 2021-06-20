eda7d708ada76248a2db68bad694ae91374f3946
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-20 13:35:00 +0530

    day9 tha: js added

--- day9/script.js
+++ day9/script.js
@@ -1,30 +1,30 @@
- function makingGrid(n)
- {
-     for(let i=0;i<n;i++)
-     {
-         const content = document.createElement('div');
-         content.className = 'content';
-         gridElements.appendChild(content);
-     }
- }
+function makingGrid(n)
+{
+    for(let i=0;i<n;i++)
+    {
+        const content = document.createElement('div');
+        content.className = 'content';
+        gridElements.appendChild(content);
+    }
+}
 
 
- function click(contents)
- {
-     for(let content of contents)
-     {
-         content.addEventListener('click',function () {
-             if(content.classList.contains('content_clicked')){
-                 content.classList.remove('content_clicked');
-             }
-             else{
-                 content.classList.add('content_clicked');
-             }
-         })
-     }
- }
+function click(contents)
+{
+    for(let content of contents)
+    {
+        content.addEventListener('click',function () {
+            if(content.classList.contains('content_clicked')){
+                content.classList.remove('content_clicked');
+            }
+            else{
+                content.classList.add('content_clicked');
+            }
+        })
+    }
+}
 
- const gridElements = document.querySelector('.grid');
- const contents = document.getElementsByClassName('content');
- makingGrid(36);
- click(contents);
+const gridElements = document.querySelector('.grid');
+const contents = document.getElementsByClassName('content');
+makingGrid(36);
+click(contents);
