a9a3297f91e1b660cfff2c3e44eaedbc84314fd6
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-20 15:10:00 +0530

    day9: counter added

--- day9/script.js
+++ day9/script.js
@@ -1,3 +1,5 @@
+let c = 0;
+
 function makingGrid(n)
 {
     for(let i=0;i<n;i++)
@@ -13,13 +15,21 @@ function click(contents)
 {
     for(let content of contents)
     {
-        content.addEventListener('click',function () {
+        content.addEventListener('click',function ()
+        {
+            let booked = document.getElementById('book');
+            let left = document.getElementById('left');
+
             if(content.classList.contains('content_clicked')){
                 content.classList.remove('content_clicked');
+                c--;
             }
             else{
                 content.classList.add('content_clicked');
+                c++;
             }
+            booked.innerText = c;
+            left.innerText = 36-c;
         })
     }
 }
@@ -28,3 +38,5 @@ const gridElements = document.querySelector('.grid');
 const contents = document.getElementsByClassName('content');
 makingGrid(36);
 click(contents);
+console.log(c);
+
