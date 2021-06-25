5172b6918d18d9ec0f598b060f0e12f17716e50a
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-25 21:42:14 +0530

    day9 bug resolved

--- day9/script.js
+++ day9/script.js
@@ -37,6 +37,7 @@ function click(contents)
 const gridElements = document.querySelector('.grid');
 const contents = document.getElementsByClassName('content');
 
+makingGrid(36)
 click(contents);
 console.log(c);
 
