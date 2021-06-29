a342d11ebe5c5e25859da6295f81ac3e6c423224
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-30 01:35:34 +0530

    card flip added

--- day10/script.js
+++ day10/script.js
@@ -1,5 +1,5 @@
 let grid = document.querySelector('.grid');
-
+let c=0;
 function makingGrid(n)
 {
     for(let i=0;i<n;i++)
@@ -7,16 +7,37 @@ function makingGrid(n)
         const content = document.createElement('div');
         content.className = 'content';
         grid.appendChild(content);
-        const content_inner = document.createElement('div');
-        content_inner.className = 'content_inner';
-        content.appendChild(content_inner);
-        const content_front = document.createElement('div');
-        const content_back = document.createElement('div');
-        content_front.className = 'content_front';
-        content_back.className = 'content_back';
-        content_inner.appendChild(content_front);
-        content_inner.appendChild(content_back);
+        const card = document.createElement('div');
+        card.className = 'card';
+        content.appendChild(card);
+        const card_front = document.createElement('div');
+        const card_back = document.createElement('div');
+        card_front.classList.add('card_face');
+        card_front.classList.add('card_front');
+        card_back.classList.add('card_face');
+        card_back.classList.add('card_back');
+        card.appendChild(card_front);
+        card.appendChild(card_back);
     }
 }
 
+function doFlip() {
+    for (let card of cards) {
+        card.addEventListener('click', function () {
+            card.style.transform = "rotateY(180deg)";
+            c++;
+        });
+    }
+}
+
+
 makingGrid(24);
+let cards = document.querySelectorAll('.card');
+doFlip();
+
+// if(c===2) {
+//     for(let card of cards) {
+//         card.style.transform = "rotateY(180deg)";
+//         c = 0;
+//     }
+// }
